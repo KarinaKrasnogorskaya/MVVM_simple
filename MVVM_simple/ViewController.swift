25be99ebbf98878805a9371c042a88b199998072
KarinaKR00 <kr_karina00@mail.ru>
2023-02-28 18:32:18 +0300

    Initial Commit

---
+++ MVVM_simple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_simple
+//
+//  Created by Карина on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
